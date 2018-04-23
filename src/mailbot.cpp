2bfb96efa58786f26c59cf7da776e236020f5ca4
Margo Urheim <38016278+murheim@users.noreply.github.com>
2018-04-23 10:15:52 -0400

    began yaml reader

--- src/mailbot.cpp
+++ src/mailbot.cpp
@@ -1,2 +1,18 @@
-#include<ros/ros.h>
-#include<yaml-cpp>
+#include <ros/ros.h>
+#include<iostream>
+#include<yaml-cpp/yaml.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  
+  YAML::Node config = YAML::LoadFile(argv[1]);
+  YAML::Node& profs = confg["Professors"];
+  YAML::Node& rooms = confg["Rooms"];
+
+/*for (std::size_t i = 0; i < node_test1.size(); i++) {
+    const YAML::Node& node_test2 = node_test1[i];
+    std::cout << "Id: " << node_test2["id"].as<std::string>() << std::endl;
+    std::cout << "hardwareId: " << node_test2["hardwareId"].as<std::string>() << std::endl << std::endl;*/
+}
